bb95f80523e1b0f66c2abf3d63f307939f97c306
Chege Qumu <chege.qumu@shopee.com>
2022-03-11 18:36:32 +0800

    code optimise

--- lib/login/github.ts
+++ lib/login/github.ts
@@ -24,27 +24,13 @@ export const cacheGithubAuthInfo = ({
 export const getCachedGithubAuthInfo = () => {
   return github_auth_info
 }
-const github_auth_clientid = process.env.github_auth_clientid || 'a3038c6aecde7bd31b6e'
-// console.log('github_auth_clientid', github_auth_clientid, process.env.github_auth_clientid)
-const HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://blog-comment-mocha.vercel.app'
 
 const PARENT_GITHUB_AUTH_MSG_START = 'PARENT_GITHUB_AUTH_MSG_START'
-export default function openGithubAuth(callbackUrl?: string) {
+export default function openGithubAuth() {
   window.parent?.postMessage(
     JSON.stringify({
-      msg: PARENT_GITHUB_AUTH_MSG_START,
-      data: {
-        github_auth_clientid,
-        commentDeployUrlHost: HOST,
-        callbackUrl
-      }
+      msg: PARENT_GITHUB_AUTH_MSG_START
     }),
     '*'
   )
-
-  const url = `https://github.com/login/oauth/authorize?client_id=${github_auth_clientid}`
-    + `&redirect_uri=${`${HOST}/api/githubLoginCallback?redirect_url=` + (callbackUrl || window.location.href)}`
-  window.location.href = url
-    HOST + '/api/proxyIframeResponse?targetUrl='
-      + encodeURIComponent(url)
 }
